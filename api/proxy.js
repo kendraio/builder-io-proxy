8fb610646663f89733176eafdb507475a07e4541
darren <darren@darrenmothersele.com>
2021-02-04 13:08:06 +0000

    init

---
+++ api/proxy.js
@@ -0,0 +1,20 @@
+const axios = require('axios');
+const _ = require('lodash');
+const apiKey = process.env.BUILDER_API_KEY;
+
+const template = ({ title, html }) => `<html lang="en">
+<head><title>${title}</title></head>
+<body>${html}</body>
+</html>
+`;
+
+module.exports = async (req, res) => {
+    const pageSlug = _.get(req, 'query.path', 'home');
+    const url = `https://cdn.builder.io/api/v1/html/page?url=/${encodeURIComponent(pageSlug)}&apiKey=${apiKey}`;
+    try {
+        const {data} = await axios(url);
+        res.send(template(data.data));
+    } catch (e) {
+        res.status(500).send(e.message);
+    }
+}
