cabed8921c9c87d13581f9fb41fbfb47a534bf67
Darren Mothersele <darren@darrenmothersele.com>
2021-06-10 08:16:18 +0100

    update proxy to read profile data from user.kendra.io

--- api/proxy.js
+++ api/proxy.js
@@ -9,7 +9,7 @@ module.exports = async (req, res) => {
     const url = new URL(req.url, `http://${req.headers.host}`);
     // console.log(url.pathname);
     const isOldSite = url.pathname.substring(0, 5) === '/user' || url.pathname.substring(0, 5) === '/orgs';
-    const target = isOldSite ? `https://kendra.io` : `http://kendraio-builder-website.s3-website.eu-west-2.amazonaws.com`;
+    const target = isOldSite ? `https://user.kendra.io` : `http://kendraio-builder-website.s3-website.eu-west-2.amazonaws.com`;
     //console.log(target);
     proxy.web(req, res, { target });
 };
