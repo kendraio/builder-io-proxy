5ec35b0af4ee6db17e9b447f8e74353a131905a7
Darren Mothersele <darren@darrenmothersele.com>
2021-06-10 09:28:47 +0100

    add sites and misc to path for proxying

--- api/proxy.js
+++ api/proxy.js
@@ -8,8 +8,10 @@ const proxy = httpProxy.createProxyServer({
 module.exports = async (req, res) => {
     const url = new URL(req.url, `http://${req.headers.host}`);
     // console.log(url.pathname);
-    const isOldSite = url.pathname.substring(0, 5) === '/user' || url.pathname.substring(0, 5) === '/orgs';
+    const isOldSite = url.pathname.substring(0, 5) === '/user' || url.pathname.substring(0, 5) === '/orgs'
+        || url.pathname.substring(0, 6) === '/sites'
+        || url.pathname.substring(0, 5) === '/misc';
     const target = isOldSite ? `https://user.kendra.io` : `http://kendraio-builder-website.s3-website.eu-west-2.amazonaws.com`;
     //console.log(target);
-    proxy.web(req, res, { target });
+    proxy.web(req, res, {target});
 };
